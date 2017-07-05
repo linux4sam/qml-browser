85021e94f3545465545a23000be357f6d110ed11
Joshua Henderson <joshua.henderson@microchip.com>
2017-07-05 16:47:58 -0700

    Fix issue with relative path being converted to URL

--- resources/microchip.sh
+++ resources/microchip.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 cd "$(dirname "$0")/.."
-bin/browser -plugin tslib:/dev/input/event1 -plugin evdevkeyboard:/dev/input/event0 -url='../../resources/microchip_offline.html'
+bin/browser -plugin tslib:/dev/input/event1 -plugin evdevkeyboard:/dev/input/event0 -url='file:///opt/browser/resources/microchip_offline.html'
