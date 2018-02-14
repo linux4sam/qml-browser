0626a3e087940bca6ced55ceee0df0074c0e9fee
Joshua Henderson <joshua.henderson@microchip.com>
2018-02-13 17:26:06 -0700

    Rework for new launch method

--- resources/browser.sh
+++ resources/browser.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+eval "$DEMO_LEAVE"
+
 cd "$(dirname "$0")/.."
 bin/browser $@
+
+eval "$DEMO_ENTER"
--- resources/microchip.sh
+++ resources/microchip.sh
@@ -1,4 +1,8 @@
-#! /bin/sh
+#!/bin/sh
+
+eval "$DEMO_LEAVE"
 
 cd "$(dirname "$0")/.."
 bin/browser -url='file:///opt/browser/resources/microchip_offline.html' $@
+
+eval "$DEMO_ENTER"
